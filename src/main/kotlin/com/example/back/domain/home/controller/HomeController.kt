a7ee6adecf1b37876b82081cc95be16c0a52cb4f
wonseokyoon <backend.wonseok@gmail.com>
2025-04-04 11:09:32 +0900

    secret 파일 추가

--- src/main/kotlin/com/example/back/domain/home/controller/HomeController.kt
+++ src/main/kotlin/com/example/back/domain/home/controller/HomeController.kt
@@ -1,14 +1,18 @@
 package com.example.back.domain.home.controller
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HomeController {
 
+    @Value("\${custom.secretValue}")
+    private lateinit var secretValue: String
+
     @GetMapping("")
     fun home(): String{
-      return "Hello World! _ V7"
+      return "Hello World! _ V7: " + secretValue
     }
 
 }
